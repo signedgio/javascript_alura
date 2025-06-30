5e22fe23114714bd7d29985e3ad18b0c08284f2e
gio <gioromanlopes2007@gmail.com>
2025-06-30 17:50:46 -0300

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,8 @@
+alert("Boas vindas ao nosso site!");
+let nome = "Lua";
+let idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+let mensagemDeErro = "Preencha todos os campos!!!";
+alert(mensagemDeErro);
+
---
+++ Untitled-2.js
@@ -0,0 +1,7 @@
+alert("Bem vindo!");
+let nome = prompt("Qual é o seu nome?");
+let idade = prompt("Quantos anos você tem?");
+
+if (idade >= 18) {
+    alert("Já pode tirar a habilitação!");
+}
