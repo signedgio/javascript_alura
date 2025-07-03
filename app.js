b49f1dd35c63870ad81b3c1cfa1fc31db1dc821b
gio 3 <gioromanlopes2007@gmail.com>
2025-07-03 17:33:26 -0300

    Add files via upload
    código "final" do curso de lógica de programação em java script da alura.

---
+++ app.js
@@ -0,0 +1,30 @@
+alert("Boas vindas ao jogo do número secreto!");
+let numeroMaximo = 5000;
+let numeroSecreto = parseInt(Math.random() * 5000 + 1);
+// let chute = prompt("Escolha um número entre 1 e 30"); (tiramos pra colocar o while)
+let chute;
+let tentativas = 1;
+
+while (chute != numeroSecreto) {
+    chute = prompt("Escolha um número entre 1 e " + numeroMaximo);
+    if (numeroSecreto == chute) {
+        break;
+    } else {
+    if (numeroSecreto > chute) {
+        alert("O número secreto é maior que " + chute);
+    } else {
+        alert("O número secreto é menor que  " + chute);
+    }
+    // tentativas = tentativas + 1;
+    tentativas++;
+    }
+}
+// uma outra maneira de escrever sem if e else:
+// let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+// alert("ACERTOU! Muito bem! o número secreto é " + numeroSecreto + " e você acertou com " + tentativas + palavraYentativa);
+// (se tiver mais de uma tentativa = "tentativas". se não, "tentativa".)
+if (tentativas > 1) {
+    alert("ACERTOU! Muito bem! o número secreto é " + numeroSecreto + " e você acertou com " + tentativas + " tentativas.");
+} else {
+    alert("ACERTOU! Muito bem! o número secreto é " + numeroSecreto + " e você acertou com " + tentativas + " tentativa.");
+}
