310551a294230e8fb7ad1724381017bceefb3bd6
gio 3 <gioromanlopes2007@gmail.com>
2025-07-03 17:39:51 -0300

    Update ex4_aula3.js

---
+++ ex4_aula3.js
@@ -0,0 +1,10 @@
+// Crie um programa de contagem progressiva. 
+// Peça um número e conte de 0 até esse número, usando um loop while no console do navegador.
+
+let contagemRegressiva = 0
+let inicio = prompt("Escolha um número para a contagem regressiva.");
+
+while (contagemRegressiva <= inicio) {
+    alert(contagemRegressiva);
+    contagemRegressiva ++
+}
